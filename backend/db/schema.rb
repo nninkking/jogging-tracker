4bde045aa3430c469e6fbe0870cfd01c395ccaeb
RichardDeimos <richarddeimos@yandex.com>
2019-04-07 07:52:59 -0700

    report_function finished

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -24,11 +24,6 @@ ActiveRecord::Schema.define(version: 2019_04_06_160006) do
     t.index ["user_id"], name: "index_records_on_user_id"
   end
 
-  create_table "reports", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "firstname"
